b513f8183e6b4a291912dfb6531a832c678c1618
Al Amin Ahamed <34349365+mralaminahamed@users.noreply.github.com>
2023-06-03 09:06:13 +0600

    worked with program 05

---
+++ class-02-tasks/program_05.py
@@ -0,0 +1,12 @@
+# Show message to the screen for getting radius of a circle.
+radius = int(input('The radius of circle: '))
+
+# Set the default value for pi
+pi = 3.1416
+
+# Calculate the area of a circle. and assign variable.
+# formula: A=πr2
+area = pi * (radius * radius)
+
+# Show the output.
+print("The area of a circle is : ", area)
