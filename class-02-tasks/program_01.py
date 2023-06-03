eb29b5c8dd7fff66645a48aadb67469b1d654f27
Al Amin Ahamed <34349365+mralaminahamed@users.noreply.github.com>
2023-06-03 08:16:08 +0600

    Worked with program 01

---
+++ class-02-tasks/program_01.py
@@ -0,0 +1,16 @@
+# Show message to the screen for getting two input numbers.
+number1 = int(input('Enter your first number: '))
+number2 = int(input('Enter your second number: '))
+
+# Calculate this two number into addition, subtraction, multiplication and division,
+# and assign separate variables.
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+division = number1 / number2
+
+# Show the output in the separate lines.
+print("The addition is : ", addition)
+print("The subtraction is : ", subtraction)
+print("The multiplication is : ", multiplication)
+print("The division is : ", division)
