56e357b0cb77a69a59893c464030ca42e0a85b64
Al Amin Ahamed <34349365+mralaminahamed@users.noreply.github.com>
2023-07-27 23:39:04 +0600

    Update gpa.py

--- class-07-tasks/gpa.py
+++ class-07-tasks/gpa.py
@@ -33,21 +33,56 @@ def gpa_calculate(final_marks):
     return gpa
 
 
+def contains_comma(input_string):
+    return ',' in input_string
+
+
 if __name__ == '__main__':
-    # Collect input from the professor
-    attendance_mark = int(input("Enter the attendance mark (out of 10): "))
-    quiz_marks = input("Enter the quiz mark (out of 15) with comma: ")
-    lab_mark = int(input("Enter the lab mark (out of 10): "))
-    final_mark = int(input("Enter the final mark (out of 50): "))
+    try:
+        # Collect input from the professor
+        attendance_mark = int(input("Enter the attendance mark (out of 10): "))
+        quiz_marks = input("Enter the quiz mark (out of 15) with comma: ")
+        lab_mark = int(input("Enter the lab mark (out of 10): "))
+        final_mark = int(input("Enter the final mark (out of 50): "))
 
-    # Split the quiz mark using the comma separator
-    values_list = quiz_marks.split(',')
+        print("-----------------------------------------------")
 
-    # Convert elements to integers using list comprehension
-    int_values_list = [int(value) for value in values_list]
+        if attendance_mark > 10:
+            print(f"Invalid attendance mark({attendance_mark}). Marks will be not greater than 10.")
 
-    # Calculate GPA
-    gpa_result = calculate_result(attendance_mark, *int_values_list, lab_mark, final_mark)
+        if lab_mark > 10:
+            print(f"Invalid lab mark({lab_mark}). Marks will be not greater than 10.")
 
-    # Show result
-    print(f"GPA for this student is {gpa_result:.2f}")
+        if final_mark > 50:
+            print(f"Invalid lab mark({final_mark}). Marks will be not greater than 50.")
+
+        if contains_comma(quiz_marks):
+            # Split the quiz mark using the comma separator
+            values_list = quiz_marks.split(',')
+
+            # Convert elements to integers using list comprehension
+            int_values_list = [int(value) for value in values_list]
+            quiz_marks_valid = 1
+
+            for quz_mark in int_values_list:
+                if quz_mark > 15:
+                    quiz_marks_valid = 0
+                    print(f"Invalid quiz mark({quz_mark}). Marks will be not greater than 15.")
+
+            if quiz_marks_valid == 1:
+                # Total Marks
+                in_course_mark = in_course_calculate(attendance_mark, *int_values_list, lab_mark)
+                final_marks = final_calculate(in_course_mark, final_mark)
+
+                # Calculate GPA
+                gpa_result = gpa_calculate(final_marks)
+
+                # Show result
+                print(f"Total mark for this student is {int(final_marks)}")
+                print(f"GPA for this student is {gpa_result:.2f}")
+
+        else:
+            print("Invalid quiz mark. QUiz mark will be contains command (,).")
+
+    except:
+        print("An exception occurred")
