6556fbc408ae4527888322698d62965538cdcf3b
Al Amin Ahamed <34349365+mralaminahamed@users.noreply.github.com>
2023-08-15 21:22:15 +0600

    Create list.py

---
+++ python-class/list.py
@@ -0,0 +1,23 @@
+lists = ['physics', 'chemistry', 1997, 2000]
+lists[0] = 'teswt'
+del lists[0]
+print(lists)
+
+lists.insert(1, 1455)
+value = lists.pop(0)
+length = len(lists)
+print(length)
+
+print(value)
+print(lists)
+
+voter = {'name': 'tewst', }
+if voter['name'] == 'test':
+    print(voter)
+
+
+
+# • list[2] = 2001;
+# • print (list[2])
+# • print (list[2])
+# • list[2:4] = [2001 2004];
